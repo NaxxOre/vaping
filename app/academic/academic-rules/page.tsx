41da40d50a6d6388e880be1c5552d95063f780f8
Naxx_g <thufa2179@gmail.com>
2024-12-17 15:02:18 +0630

    error in pdf

---
+++ app/academic/academic-rules/page.tsx
@@ -0,0 +1,24 @@
+import PdfView from './pdfview';
+import ImageHeader from "@/components/imageheader/header";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Academic-Rules UIT",
+    description: "Learn about UIT's Academic Rules",
+};
+
+const AcademicRule = () => {
+  // Directly reference the file path from the public folder
+  const pdfFile = '/AcademicRules.pdf';
+
+  return (
+    <div className="academicrules">
+      <ImageHeader url="/images/bg.jpg" name="Academic Rules" />
+      <div className="px-[5%]">
+        <PdfView file={pdfFile} />
+      </div>
+    </div>
+  );
+};
+
+export default AcademicRule;
